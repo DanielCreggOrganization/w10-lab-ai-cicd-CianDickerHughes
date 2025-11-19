ac828f571eb76b568397c283d8d9800be8da04e3
Daniel Cregg <danielcregg@users.noreply.github.com>
2025-11-19 16:44:04 +0000

    Remove AWS_SECRET_KEY definition from webapp.py

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -4,5 +4,3 @@ from . import views  # For import side-effects of setting up routes.
 
 # Time-saver: output a URL to the VS Code terminal so you can easily Ctrl+click to open a browser
 # print('http://127.0.0.1:5000/hello/VSCode')
-# In hello_app/webapp.py
-AWS_SECRET_KEY = "AKIA1234567890"
